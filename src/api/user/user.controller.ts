a8e7dbb0c0a1fe8283335e82660a38e03fab26fc
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-11-07 09:00:01 +0700

    chore: remove console log

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,17 +1,11 @@
 import { Controller, Get, Request } from '@nestjs/common';
-import { UserService } from './user.service';
-import { ConfigService } from '@nestjs/config';
 
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly userService: UserService,
-    private configService: ConfigService,
-  ) {}
+  constructor() {}
 
   @Get()
   getMe(@Request() req) {
-    console.log(this.configService.get('token'));
     return req.user;
   }
 }
